27341fca411a89fd50b65a23e347e162654957ec
Cristian Moldovan <cristianmldvn@MacBook-Pro.local>
2019-12-10 16:47:06 +0200

    Editied db string

--- grocery-store/src/main/kotlin/com/grocerystore/grocerystore/controllers/AisleController.kt
+++ grocery-store/src/main/kotlin/com/grocerystore/grocerystore/controllers/AisleController.kt
@@ -5,7 +5,9 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import com.grocerystore.grocerystore.repos.AisleRepository
 import com.grocerystore.grocerystore.models.Aisle
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.ResponseBody
 
 @RestController
 @RequestMapping("/api")
